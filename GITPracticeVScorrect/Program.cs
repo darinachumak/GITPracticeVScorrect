e31a072105a50f460e6ef0b74222ac9a106aa87c
darinachumak <darinachumak@gmail.com>
2023-10-13 18:18:11 +0300

    fix

--- GITPracticeVScorrect/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World again!");
-Console.WriteLine("Make a new commit!");
-Console.WriteLine("My new changes");
